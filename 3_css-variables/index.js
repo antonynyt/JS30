bc2ee1e15acd8fdb3c364b3595c9fc7d9912851d
Antony <antony.neyret@gmail.com>
2022-03-28 00:49:05 +0200

    add draggable controls

--- 3_css-variables/index.js
+++ 3_css-variables/index.js
@@ -9,3 +9,37 @@ function update(){
 inputs.forEach( input => {
     input.addEventListener("change", update)
 })
+
+
+const dragElem = document.querySelector('#header')
+const moveElem = document.querySelector('.border__control')
+
+dragElem.addEventListener("mousedown", dragable )
+
+function dragable(eventObj){
+    eventObj.preventDefault()
+
+    let clickMouseX = eventObj.clientX
+    let clickMouseY = eventObj.clientY
+
+    document.onmousemove = (e) => {
+        e.preventDefault()
+
+        let mouseX = e.clientX - clickMouseX
+        clickMouseX = e.clientX
+
+        let mouseY = e.clientY - clickMouseY
+        clickMouseY = e.clientY
+
+        moveElem.style.left = (moveElem.offsetLeft) + mouseX + 'px'
+        moveElem.style.top = (moveElem.offsetTop) + mouseY + 'px'
+
+    }
+
+    document.onmouseup = () => {
+        document.onmouseup = null
+        document.onmousemove = null
+    }
+}
+
+
