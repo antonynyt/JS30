7353e47f5c6f1ee30aa38f72b19840bc18e88ea0
Antony <antony.neyret@gmail.com>
2022-04-04 23:55:29 +0200

    day10 Add feature to btn new task

--- 10_shift-todo/index.js
+++ 10_shift-todo/index.js
@@ -1,12 +1,10 @@
-const checkboxes = document.querySelectorAll('.checkbox')
-
-console.log(checkboxes)
-
+let checkboxes = document.querySelectorAll('.checkbox')
 let lastChecked
 
-function shiftCheck(e){
+function handleCheck(e){
     let inBetween = false
 
+    //check all if shift is checked
     if(e.shiftKey && this.checked){
         checkboxes.forEach(checkbox => {
             if (checkbox === this || checkbox === lastChecked){
@@ -19,6 +17,7 @@ function shiftCheck(e){
         })
     }
 
+    //uncheck 
     if(e.shiftKey && this.checked === false){
         checkboxes.forEach(checkbox => {
             if (checkbox === this || checkbox === lastChecked){
@@ -34,7 +33,31 @@ function shiftCheck(e){
 }
 
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', shiftCheck)
+    checkbox.addEventListener('click', handleCheck)
 })
 
 
+const taskList = document.querySelector('.task__list')
+
+function addTodo(){
+    const task = document.createElement('li')
+    task.classList.add('task__el')
+
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.classList.add('checkbox')
+
+    const text = document.createElement('p')
+    text.setAttribute('contenteditable', 'true')
+    text.classList.add('task__text')
+
+    taskList.append(task)
+    task.append(checkbox)
+    task.append(text)
+
+    text.focus()
+    checkboxes = document.querySelectorAll('.checkbox')
+}
+
+const addBtn = document.querySelector('#task__add-btn')
+addBtn.addEventListener('click', addTodo)
