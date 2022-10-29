c45fc60a7b3697c079041bda6da045857c10936c
Antony <antony.neyret@gmail.com>
2022-10-29 17:20:31 +0200

    Add Ambi-light + local video file

--- 11_html-video-player/index.js
+++ 11_html-video-player/index.js
@@ -1,3 +1,4 @@
+//player
 const playIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg>'
 const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z"/></svg>'
 const skipForwardIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="27.5" viewBox="0 0 24 27.5"><path d="M23.5,11.9l-2,0.5c1.1,3.5,0.3,7.4-2.4,10.1c-3.9,3.9-10.2,3.9-14.1,0S1,12.3,4.9,8.4C6.9,6.5,9.4,5.5,12,5.5V9l5.9-4.6L12,0v3.5C8.9,3.5,5.9,4.7,3.5,7c-4.7,4.7-4.7,12.3,0,17c4.7,4.7,12.3,4.7,17,0C23.7,20.7,24.7,16.1,23.5,11.9z"/><path d="M9.6,20v-9H8c0,1.2-0.6,1.5-2.3,1.5V14h1.9V20H9.6z"/><path d="M14.4,10.9c-2.5,0-3.8,1.8-3.8,4.6c0,2.8,1.3,4.6,3.8,4.6c2.5,0,3.8-1.8,3.8-4.6C18.3,12.7,16.9,10.9,14.4,10.9zM14.4,18.5c-1.2,0-1.8-1.1-1.8-3s0.5-3,1.8-3c1.2,0,1.8,1.1,1.8,3S15.7,18.5,14.4,18.5z"/></svg>'
@@ -19,6 +20,22 @@ const statusDiv = player.querySelector('.status')
 const fullscreen = player.querySelector('.fullscreen')
 const playerControls = player.querySelector('.player__controls')
 
+//load the file
+const fileChooser = document.querySelector('.choose__file')
+const fileInput = document.querySelector('#file-btn')
+
+function playFile(){
+    let file = this.files[0];
+    let fileURL = URL.createObjectURL(file);
+    video.src = fileURL;
+    fileChooser.style.display = 'none'
+    video.addEventListener('play', init, false)
+}
+
+fileInput.addEventListener('change', playFile, false)
+
+
+//toggle play
 function togglePlay() {
     if(video.paused){
         video.play()
@@ -198,7 +215,7 @@ function controlsColor(){
     rgbSums[2] = Math.floor(rgbSums[2]/pixelCount)
     
     // Set the background color to the new color
-    let newRgb = 'rgb(' + rgbSums.join(',') + ')'
+    let newRgb = '' + rgbSums.join(',') + ''
     document.documentElement.style.setProperty('--mainColor', newRgb)
     setTimeout(controlsColor, 100);
 }
